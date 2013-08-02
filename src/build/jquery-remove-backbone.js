d9733f144e7c88a98a9a308bb9bc1391d25ac9b4
Justin Ridgewell <justin@ridgewell.name>
2013-08-02 19:36:58 -0400

    Add necessary build files
    My personal .gitignore ignored them...

---
+++ src/build/jquery-remove-backbone.js
@@ -0,0 +1,13 @@
+(function(root, _, $, Backbone) {
+    'use strict';
+
+// @include ../jquery.cleanData.js
+// @include ../backbone.remove.js
+
+    if (typeof exports === 'undefined') {
+        var exports = root;
+    }
+    exports.Backbone = Backbone;
+    exports.jQuery = jQuery;
+
+})(this, _, jQuery, Backbone);
